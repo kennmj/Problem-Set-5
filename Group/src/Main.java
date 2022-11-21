b5b576ed70f5d13c504f2005cd5a23fa92eb01cb
colinkrinsky <krincj20@wfu.edu>
2022-11-21 14:53:44 -0500

    With speed level 2

--- Group/src/Main.java
+++ Group/src/Main.java
@@ -1,16 +1,14 @@
 import javax.swing.*;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args) {
         JFrame obj = new JFrame();
-        GamePlay gameplay = new GamePlay();
-        obj.setBounds(10, 10, 700, 600);
-        obj.setTitle("Breaker Ball");
-//        obj.setResizable(false);
+        Gameplay gamePlay = new Gameplay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        obj.add(gameplay);
+        obj.add(gamePlay);
         obj.setVisible(true);
-
-
     }
 }
